17272cbf8b9f728cff6a4ce54dc447c7f5e2a040
Adetugboboh Damilola <dadetugboboh1@gmail.com>
2023-08-18 12:50:38 +0100

    Add files via upload

---
+++ sample_project/src/main/java/org/interswitch/Calculator.java
@@ -0,0 +1,43 @@
+package org.interswitch;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static double add(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    public static double multiply(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public static double divide(double num1, double num2) {
+        if (num2 == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return num1 / num2;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter operand 1: ");
+        double operand1 = scanner.nextDouble();
+
+        System.out.print("Enter operand 2: ");
+        double operand2 = scanner.nextDouble();
+
+        scanner.close();
+
+
+        System.out.println("Addition: " + add(operand1, operand2));
+        System.out.println("Subtraction: " + subtract(operand1, operand2));
+        System.out.println("Multiplication: " + multiply(operand1, operand2));
+        System.out.println("Division: " + divide(operand1, operand2));
+    }
+
+}
